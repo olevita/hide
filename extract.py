d67bde6325ea3fa2d50584b2c3da08ae141596cc
Alex Proseno <alex.proseno@gmail.com>
2022-11-23 22:27:28 +0200

    init commit

---
+++ extract.py
@@ -0,0 +1,39 @@
+import os
+import wave
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', help='audiofile', dest='audiofile')
+args = parser.parse_args()
+af = args.audiofile
+arged = False
+
+if af:
+    arged = True
+
+
+def help():
+  print("Extract Your Secret Message from Audio Wave File.")
+  print ('''usage: extract.py [-h] [-f AUDIOFILE]
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -f AUDIOFILE  Select Audio File''')
+
+def ex_msg(af):
+    if not arged:
+      help()
+    else:
+        print ("Please wait...")
+        waveaudio = wave.open(af, mode='rb')
+        frame_bytes = bytearray(list(waveaudio.readframes(waveaudio.getnframes())))
+        extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+        string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
+        msg = string.split("###")[0]
+        print("Your message is: " + msg)
+        waveaudio.close()
+
+try:
+  ex_msg(af)
+except:
+  print ("Something went wrong!! try again")
+  quit('')
